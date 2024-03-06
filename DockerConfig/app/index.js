fe97483fade37e19da0fa12910d721f1c4c351cf
caragea.denis1806 <caragea.denis1806@gmail.com>
2024-03-06 13:15:25 +0200

    modified the index.js

--- DockerConfig/app/index.js
+++ DockerConfig/app/index.js
@@ -1,6 +1,6 @@
 const app = require("express")();
 
 app.get("/", (req, res) => 
-res.send("docker container web app"));
+res.send("Docker container web app running on external port ${process.env.PORT || internalPort}"));
 
 app.listen(9999, ()=> console.log("Listening on 9999"))
